9da5227293bedbf6a8449a9316e0fd0de82737e0
Gabriel Cenciati <gbrcenciati@gmail.com>
2023-03-26 13:37:37 -0300

    add: response decorator

--- src/api/routes/root_router.py
+++ src/api/routes/root_router.py
@@ -1,10 +1,25 @@
+from http import HTTPStatus
+
+# pylint: disable=unused-argument
 from typing import Dict
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Request, status
+
+from src.api.schemas import construct_response
 
 root_router = APIRouter()
 
 
-@root_router.get("/", status_code=status.HTTP_200_OK, response_model=Dict[str, list])
-def root():
-    return {"data": [{"Go to:": "/predict"}]}
+@root_router.get(
+    "/",
+    status_code=status.HTTP_200_OK,
+    response_model=Dict[str, str | Dict[str, str]],
+)
+@construct_response
+def _root(request: Request):
+    """Health check."""
+    return {
+        "message": HTTPStatus.OK.phrase,
+        "status-code": status.HTTP_200_OK,
+        "data": {"Go to": "/predict"},
+    }
